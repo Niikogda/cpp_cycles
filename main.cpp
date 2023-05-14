1e3b60711a1b7449329672fc5421d82d51494f72
Niikogda <niikogda@gmail.com>
2023-05-14 09:56:30 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int nums;
+	cout << "Ведіть останнє число списку-> " << endl;
+	cin >> nums;
+		do
+		{
+			i++;
+
+		}
+		while (i != nums);
+	return()
+}
